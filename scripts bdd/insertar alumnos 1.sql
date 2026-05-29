f9f2517d94bb462ade1e548effcfb1f996ee3213
Bassmin <benjapuerta@gmail.com>
2026-05-29 19:38:52 -0400

    Cambio en insert con descripcion incluida

--- scripts bdd/insertar alumnos 1.sql
+++ scripts bdd/insertar alumnos 1.sql
@@ -4,7 +4,7 @@ INSERT INTO gestion_escolar.alumnos (nro_matricula, nombre, apellidos, curso) VA
 INSERT INTO gestion_escolar.alumnos (nro_matricula, nombre, apellidos, curso) VALUES (202602, 'Sofía', 'Morales', '4to Medio B');
 
 -- Registrar un conflicto en gestion_conflictos a cargo del funcionario 1 (Juan Pérez)
-INSERT INTO gestion_conflictos.casos_conflicto (estado_caso, id_funcionario_cargo) VALUES ('en proceso', 1);
+INSERT INTO gestion_conflictos.casos_conflicto (descripcion, estado_caso, id_funcionario_cargo) VALUES ('Discusión ocurrida durante una actividad escolar.','en proceso', 1);
 
 -- Asociar los dos alumnos al conflicto número 1
 INSERT INTO gestion_conflictos.detalle_alumnos_conflicto (id_conflicto, nro_matricula_alumno) VALUES (1, 202601);
