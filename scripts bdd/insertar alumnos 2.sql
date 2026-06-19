f164372eac8784fc44e4bd44a97e56abdd101706
Xavi <pegajardo2021@udec.cl>
2026-06-18 21:59:02 -0400

    Scripts de inserción compactados en uno solo

--- scripts bdd/insertar alumnos 2.sql
+++
@@ -1,6 +0,0 @@
--- Insertar datos maestros en gestion_escolar
-INSERT INTO gestion_escolar.funcionarios (nombres, apellidos) VALUES ('Pepito', 'Sech');
-INSERT INTO gestion_escolar.funcionarios (nombres, apellidos) VALUES ('Ramón', 'Jamón');
-INSERT INTO gestion_escolar.alumnos (nro_matricula, nombre, apellidos, curso) VALUES (202603, 'Giuseppe', 'Peazo', '1ro Medio A');
-INSERT INTO gestion_escolar.alumnos (nro_matricula, nombre, apellidos, curso) VALUES (202604, 'Ana', 'Carrascal', '2do Medio A');
-INSERT INTO gestion_escolar.alumnos (nro_matricula, nombre, apellidos, curso) VALUES (202605, 'Fernando', 'Zlatanpedri', '8vo Básico A');
